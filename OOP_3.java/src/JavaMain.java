3039f00cbb08b6f7cb95398838f759a928381896
MihreteabNahom <47663091+MihreteabNahom@users.noreply.github.com>
2019-04-01 11:54:33 +0300

    Add files via upload

---
+++ OOP_3.java/src/JavaMain.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JavaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s= new Scanner(System.in);
+		ArrayList<Students> Students = new ArrayList<Students>();
+		Students sn=null;
+		
+		System.out.println("Enter students details"+" (Press 1 to continue)");
+		int num = s.nextInt();
+				int i = 0;
+		while(true) {
+			if(i>=num)
+			   break;
+			else
+				sn=new Students(); 
+				sn.setScores();
+				Students.add(sn);
+				System.out.println("////////////////////////////////////////////////////////////// ");
+				  sn.printStdInfo();
+	                sn.printScores();
+	       
+	        }
+	        i++;
+		}
+	}
+	
+
+	
